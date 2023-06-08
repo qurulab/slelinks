8b89022a55c50ee19ed7867b83da5ea84cb697a5
Achilles45 <achillesusuoyibo7@gmail.com>
2023-06-08 15:51:49 +0100

    feat: initial commit

---
+++ app/auth/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./sign-in.module.scss"
+
+const Page = () => {
+  return (
+    <div className={ styles["sign__in"] }>This is the sign in page of the app</div>
+  )
+}
+
+export default Page
